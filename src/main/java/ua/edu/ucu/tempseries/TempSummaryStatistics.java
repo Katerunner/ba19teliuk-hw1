256336e10c3fb11ca325ed3cb9ed5f09a6281aaf
Артемій <44300059+Katerunner@users.noreply.github.com>
2019-10-16 22:46:51 +0300

    Update TempSummaryStatistics.java

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -1,26 +1,10 @@
 package ua.edu.ucu.tempseries;
 
 public class TempSummaryStatistics {
-    public double getAvgTemp() {
-        return avgTemp;
-    }
-
-    public double getDevTemp() {
-        return devTemp;
-    }
-
-    public double getMinTemp() {
-        return minTemp;
-    }
-
-    public double getMaxTemp() {
-        return maxTemp;
-    }
-
-    private double avgTemp;
-    private double devTemp;
-    private double minTemp;
-    private double maxTemp;
+    public double avgTemp;
+    public double devTemp;
+    public double minTemp;
+    public double maxTemp;
 
     public TempSummaryStatistics(double avgTemp,
                                  double devTemp,
